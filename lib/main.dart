bb27877d37838340ee8fd8584334304cb3c875bc
Vrynd <vryndhika@gmail.com>
2025-09-18 09:11:24 +0700

    Integrasi firebase dengan flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:skrining_apps/firebase_options.dart';
+import 'package:skrining_apps/themes/theme_apps.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final pref = await SharedPreferences.getInstance();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -6,15 +19,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Skrining Apps',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: ThemeApps.lightTheme,
+      darkTheme: ThemeApps.darkTheme,
+      themeMode: ThemeMode.system,
     );
   }
 }
-
-void main() {
-  runApp(const MyApp());
-}
-
-
