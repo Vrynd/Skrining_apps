31034d253ea7c9c4bff09abc84f03463d7aa6fc0
Vrynd <vryndhika@gmail.com>
2025-09-20 07:11:06 +0700

    Membuat user interface login

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:skrining_apps/firebase_options.dart';
+import 'package:skrining_apps/screens/auth/login_screen.dart';
+import 'package:skrining_apps/screens/routes/route_screen.dart';
 import 'package:skrining_apps/themes/theme_apps.dart';
 
 void main() async {
@@ -9,6 +12,15 @@ void main() async {
   final pref = await SharedPreferences.getInstance();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.transparent,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ),
+  );
 
   runApp(const MyApp());
 }
@@ -18,10 +30,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Skrining Apps',
       theme: ThemeApps.lightTheme,
       darkTheme: ThemeApps.darkTheme,
-      themeMode: ThemeMode.system,
+      initialRoute: RouteScreen.login.name,
+      routes: {
+        RouteScreen.login.name: (context) => const LoginScreen(),
+        RouteScreen.home.name: (context) => const LoginScreen(),
+      },
     );
   }
 }
---
+++ lib/screens/routes/route_screen.dart
@@ -0,0 +1,8 @@
+enum RouteScreen {
+  login('login'),
+  register('register'),
+  home('home');
+
+  final String name;
+  const RouteScreen(this.name);
+}
